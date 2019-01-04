794b0d9d7d31cf0d482b5b1edadffcefb0ea115f
ivanchen20 <ivanchen20.dif02@g2.nctu.edu.tw>
2019-01-05 00:25:34 +0800

    add rect hover effect (to be changed)

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-
+    
+    var trect = document.getElementById("trect");
+    var tlng = trect.getTotalLength();
+    trect.style.strokeDasharray = tlng;
+    trect.style.strokeDashoffset = 0;
+    
+    
     $(window).scroll(function(){              
 //        console.log("indicator activated");
         if ($(this).scrollTop() > 500) {
