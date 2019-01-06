e66fd166298be3a6675e15e7c7b80030c369eba6
ivanchen20 <ivanchen20.dif02@g2.nctu.edu.tw>
2019-01-06 18:01:45 +0800

    add text slide animation

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,43 +1,62 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     var trect = document.getElementById("trect");
     var tlng = trect.getTotalLength();
     trect.style.strokeDasharray = tlng;
     trect.style.strokeDashoffset = 0;
-    
-    
-    $(window).scroll(function(){              
-//        console.log("indicator activated");
+
+
+    $(window).scroll(function () {
+                
+        
+        
         if ($(this).scrollTop() > 500) {
             $('#indicator_scroll').fadeOut("slow");
-        }else{
-            if($(this).scrollTop()>50){
+            $("#trect").css("animation-play-state", "running");
+//            if ($(this).scrollTop() > 7000) {
+//                $("#mySVG").css("display","none");
+//            }
+//            else{
+//                $("#mySVG").css("display","");
+//            }
+        } else {
+            if ($(this).scrollTop() > 50) {
                 $("#trect").css("animation-play-state", "running");
             }
             $('#indicator_scroll').fadeIn('slow');
         }
-    }); 
-    
-    
-    
-    
+
+        // text slide animation
+        $(".slideanim").each(function () {
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + window.innerHeight*0.8) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+
+
+
+
     // click and hover event: Nav
-    
+
     var isNavOpen = false;
-    
-    $(".vinyl").click(function(){
-        if(isNavOpen){
+
+    $(".vinyl").click(function () {
+        if (isNavOpen) {
             $(".navlist_fake").css("display", "none");
-        }else{
-            $(".navlist_fake").css("display", "block");   
+        } else {
+            $(".navlist_fake").css("display", "block");
         }
         isNavOpen = !isNavOpen;
         console.log(isNavOpen);
     });
 
-    $(".vinyl").hover(function(){
-        $(".navlist").toggle(function(){
-        });
+    $(".vinyl").hover(function () {
+        $(".navlist").toggle(function () {});
     });
 });
 
+//$(document).ready(function(){});
